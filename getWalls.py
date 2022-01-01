7135ee7b46b153ade55b66ae7fa0a5b223c88026
Gabriel Kraft <gabriel.kraft@gmail.com>
2021-12-31 20:16:53 -0600

    Adding version with cli arguements

--- getWalls.py
+++ getWalls.py
@@ -328,7 +328,7 @@ def main():
             continue
         
         # Skip post if not HD
-        elif not isHD(post, args.minimal-width, args.minimal-height):
+        elif not isHD(post, args.minimal_width, args.minimal_height):
             print(RED + '{}) Skipping low resolution image'.format(index) + NC)
             index += 1
             continue
