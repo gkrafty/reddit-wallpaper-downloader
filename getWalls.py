86163ff5320a6441b534e44fd31067db749bc1a1
Gabriel Kraft <gabriel.kraft@gmail.com>
2022-01-01 16:35:24 -0600

    Adding version with cli arguements

--- getWalls.py
+++ getWalls.py
@@ -337,6 +337,7 @@ def main():
 
         # Skip already downloaded images
         elif alreadyDownloaded(post,title,directory):
+            print(directory)
             print(RED + '{}) Skipping already downloaded image'.format(index) + NC)
             index += 1
             continue
